73238c830bfca79ce2bff00ebd4bb9a9192bc75b
meenakshi.raina83@gmail.com <meenakshi@meenakshi-desktop.(none)>
2012-12-16 16:50:08 +0530

    Initial import

---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,13 @@
+module ProductsHelper
+def print_price(price)
+ number_to_currency price
+end
+def print_stock(stock)
+if stock > 0
+In Stock 
+else
+Out of Stock
+end
+end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merchant::Application.config.secret_token = '1db280c07ed0a644b1f5ad0c531fdec40253ac75f29846d2e8d9eeed7475822ef695a41fdd10109196ff2bd24ce38943f5bb62e9a180c0491cd822a4ec100187'
